08ee501fd6b2a6bea3c56939b30e781e13acf1dd
AestheticsPeace <63497055+AestheticsPeace@users.noreply.github.com>
2020-08-04 14:34:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -861,8 +861,7 @@ const commands = {
     embed.setThumbnail(`http://www.clker.com/cliparts/m/j/s/g/j/L/contact-me-no-data-md.png`)
     embed.setColor('#0099ff')
     embed.setTitle(`Contact Me At:`)
-    embed.setDescription(`**
-    Roblox Profile:
+    embed.setDescription(`**Roblox Profile:
     ${CONTACTINFO.RobloxProfileLink}
 
     SnapChat:
